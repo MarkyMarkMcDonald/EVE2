32ad4c9f76ab9699cc129a996c6e3db39c188606
Mark McDonald <mark.mcdonald.ga@gmail.com>
2013-12-04 23:53:21 -0500

    adding color gradients

--- js/color.js
+++ js/color.js
@@ -1,1 +1,29 @@
-// functions for creating dynamic colors go here
+var percentColors = [
+  { pct: 0.0, color: { r: 0xff, g: 0x00, b: 0 } },
+  { pct: 0.5, color: { r: 0xff, g: 0xff, b: 0 } },
+  { pct: 1.0, color: { r: 0x00, g: 0xff, b: 0 } }
+];
+
+/**
+ * Returns a (css styling) color from two gradients based on a percent - one gradient between low + mid and one between mid + high
+ * Taken from: http://stackoverflow.com/questions/7128675/from-green-to-red-color-depend-on-percentage
+ */
+var getColorForPercentage = function(pct) {
+  for (var i = 0; i < percentColors.length; i++) {
+    if (pct <= percentColors[i].pct) {
+      var lower = percentColors[i - 1];
+      var upper = percentColors[i];
+      var range = upper.pct - lower.pct;
+      var rangePct = (pct - lower.pct) / range;
+      var pctLower = 1 - rangePct;
+      var pctUpper = rangePct;
+      var color = {
+        r: Math.floor(lower.color.r * pctLower + upper.color.r * pctUpper),
+        g: Math.floor(lower.color.g * pctLower + upper.color.g * pctUpper),
+        b: Math.floor(lower.color.b * pctLower + upper.color.b * pctUpper)
+      };
+      return 'rgb(' + [color.r, color.g, color.b].join(',') + ')';
+      // or output as hex if preferred
+    }
+  }
+};
