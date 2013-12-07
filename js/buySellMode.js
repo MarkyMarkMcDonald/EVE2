45ab5f03193c9e8ed8f07851b92bb402a5c9539b
Mark McDonald <mark.mcdonald.ga@gmail.com>
2013-12-06 21:52:17 -0500

    fixed region + ordertype overlapping variables

--- js/buySellMode.js
+++ js/buySellMode.js
@@ -3,7 +3,7 @@ $(function(){
   $('.options').on('click', 'button.sell,button.buy', function(event){
     var $this = $(this);
     // check if there is a change
-    if (currentMode != $this.attr('name')) {
+    if (orderType != $this.attr('name')) {
       $this.parent('.btn-group').find('.active').removeClass('active');
       $this.addClass('active');
       orderType = $this.attr('name');
